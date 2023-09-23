10514ccb5292ff859f6264357195d880cfe2bd67
Branislav Balaz <branislav.balaz@xit.camp>
2023-09-23 16:58:51 +0200

    currency added

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,22 +1,35 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
-  const { budget } = useContext(AppContext);
-  const [newBudget, setNewBudget] = useState(budget);
-  const handleBudgetChange = event => {
-    setNewBudget(event.target.value);
+  const { budget, dispatch, expenses } = useContext(AppContext);
+
+  const changeBudget = val => {
+    const totalExpenses = expenses.reduce((total, item) => {
+      return (total += item.cost);
+    }, 0);
+
+    if (val < totalExpenses) {
+      alert("You cannot reduce the budget value lower than the spending");
+    } else {
+      dispatch({
+        type: "SET_BUDGET",
+        payload: val
+      });
+    }
   };
+
   return (
     <div className="alert alert-secondary">
-      <span>Budget: £{budget}</span>
+      <span>Budget: £</span>
       <input
         type="number"
         step="10"
-        value={newBudget}
-        onChange={handleBudgetChange}
+        value={budget}
+        onInput={event => changeBudget(event.target.value)}
       ></input>
     </div>
   );
 };
+
 export default Budget;
